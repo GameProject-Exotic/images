25c5fdd4a52d7690930c67a81ecdbffbd032f689
anominy <140955338+anominy@users.noreply.github.com>
2026-07-04 00:06:50 +0300

    Add Extract-ImageColor PowerShell script

---
+++ Extract-ImageColor.ps1
@@ -0,0 +1,171 @@
+param(
+    [Alias('SourceDirectory', 'SrcDir')]
+    [AllowNull()]
+    [AllowEmptyString()]
+    [string]$SourceDirectoryPath = [NullString]::Value,
+
+    [Alias('OutputDirectory', 'OutDir')]
+    [AllowNull()]
+    [AllowEmptyString()]
+    [string]$OutputDirectoryPath = [NullString]::Value,
+
+    [Alias('OutputFile', 'Out')]
+    [AllowNull()]
+    [AllowEmptyString()]
+    [string]$OutputFilePath = [NullString]::Value,
+
+    [Alias('ExcludeFile', 'Exclude')]
+    [AllowNull()]
+    [AllowEmptyString()]
+    [string]$ExcludeFilePath = [NullString]::Value,
+
+    [Alias('SampleCount')]
+    [AllowNull()]
+    [Nullable[int]]$SampleColorCount = $null
+)
+
+#Requires -Version 7
+
+$ErrorActionPreference = 'Stop'
+$ProgressPreference = 'Continue'
+
+Set-StrictMode -Version 3.0
+
+[Threading.Thread]::CurrentThread.CurrentCulture = [Globalization.CultureInfo]::GetCultureInfo('en-US')
+
+Import-Module -Name (Join-Path $PSScriptRoot "$([IO.Path]::GetFileNameWithoutExtension($MyInvocation.MyCommand.Name)).psm1") -Scope Local -Force
+
+
+if (-Not (Test-SourceDirectoryPath $SourceDirectoryPath)) {
+    $SourceDirectoryPath = $DefaultSourceDirectoryPath
+}
+if (-Not (Test-OutputDirectoryPath $OutputDirectoryPath)) {
+    $OutputDirectoryPath = $DefaultOutputDirectoryPath
+}
+if (-Not (Test-OutputFilePath $OutputFilePath)) {
+    $OutputFilePath = $DefaultOutputFilePath
+}
+if (-Not (Test-ExcludeFilePath $ExcludeFilePath)) {
+    $ExcludeFilePath = $DefaultExcludeFilePath
+}
+if (-Not (Test-SampleColorCount $SampleColorCount)) {
+    $SampleColorCount = $DefaultSampleColorCount
+}
+
+
+if (-Not (Test-Path -LiteralPath $SourceDirectoryPath -PathType Container)) {
+    throw Get-InvalidDirectoryPathErrorMessage(Get-NameOf { $SourceDirectoryPath })
+}
+
+$OutputFilePath = Join-Path $OutputDirectoryPath $OutputFilePath
+if (-Not (Test-Path -LiteralPath $OutputFilePath -IsValid)) {
+    throw Get-InvalidFilePathErrorMessage(Get-NameOf { $OutputFilePath })
+}
+
+[string[]]$excludes = @()
+if (Test-Path -LiteralPath $ExcludeFilePath -PathType Leaf) {
+    $excludes = [IO.File]::ReadAllLines($ExcludeFilePath) | ForEach-Object { $_.Trim() }
+    $excludes = $excludes.Where({ $_ -Ne '' })
+}
+
+[object[]]$includes = (Get-ChildItem -LiteralPath $SourceDirectoryPath -Filter '*.jpg' -File -Force)
+if ($excludes.Count -Ne 0) {
+    $includes = $includes.Where({ $_.BaseName -NotIn $excludes })
+}
+if ($includes.Count -Eq 0) {
+    Write-Warning 'He~y.. You forgot to put somw images! No~o, not the cat~ ones..'
+    return
+}
+
+$PSStyle.Progress.Style = $ProgressStyle
+$PSStyle.Progress.View = $ProgressView
+
+$kv = [Text.StringBuilder]::new()
+Write-SectionStart $kv $KeyValueColorRootSectionName
+
+[int]$progress = 0
+$includes | ForEach-Object {
+    Write-Progress -Id $ProgressImageProcessId `
+        -Activity $ProgressImageProcessActivity `
+        -Status ($ProgressImageProcessStatus -F $_.BaseName) `
+        -PercentComplete (($progress + 1) / $includes.Count * 100)
+
+    [object[]]$colors = magick $_.FullName +dither -colors $SampleColorCount -format '%c' histogram:info:- `
+        | ForEach-Object {
+            $m = $ColorHistogramRegex.Match($_)
+            if (-Not $m.Success) {
+                return
+            }
+
+            $g = $m.Groups
+
+            [int]$count = $g[$ColorHistogramCountRegexGroup].Value
+            [float]$red = $g[$ColorHistogramRedComponentRegexGroup].Value
+            [float]$green = $g[$ColorHistogramGreenComponentRegexGroup].Value
+            [float]$blue = $g[$ColorHistogramBlueComponentRegexGroup].Value
+            [float]$weight = ($red * $count + $green * $count + $blue * $count) / 3
+
+            [PSCustomObject]@{
+                Count = $count
+                Red = $red
+                Green = $green
+                Blue = $blue
+                Weight = $weight
+            }
+        }
+
+    [float]$red = 0
+    [float]$green = 0
+    [float]$blue = 0
+
+    [float]$weight = ($colors | Measure-Object Weight -Sum).Sum
+    if ($weight -Ne 0) {
+        $red = ($colors | ForEach-Object -PipelineVariable x { $_.Red * $_.Weight } | Measure-Object -Sum).Sum / $weight
+        $green = ($colors | ForEach-Object { $_.Green * $_.Weight } | Measure-Object -Sum).Sum / $weight
+        $blue = ($colors | ForEach-Object { $_.Blue * $_.Weight } | Measure-Object -Sum).Sum / $weight
+    }
+
+    [string[]]$hvals = Convert-ToHex $red $green $blue
+
+    [string]$hred = $hvals[0]
+    [string]$hgreen = $hvals[1]
+    [string]$hblue = $hvals[2]
+
+    [string]$hex = [string]::Join('', $hvals)
+
+    [string[]]$hsb = Convert-ToHsb $red $green $blue
+    [float]$hue = $hsb[0]
+    [float]$saturation = $hsb[1]
+    [float]$brightness = $hsb[2]
+
+    [int]$value = [Convert]::ToInt32($hex, 16)
+
+    Write-KeyValue $kv $_.BaseName $value
+
+    # Write-SectionStart $kv $_.BaseName
+    # # {
+    #     Write-KeyValue $kv $KeyValueColorValueKey $value
+    #     Write-KeyValue $kv $KeyValueColorHexKey $hex
+    #     Write-KeyValue $kv $KeyValueColorRedComponentKey $red
+    #     Write-KeyValue $kv $KeyValueColorGreenComponentKey $green
+    #     Write-KeyValue $kv $KeyValueColorBlueComponentKey $blue
+    #     Write-KeyValue $kv $KeyValueColorRedHexComponentKey $hred
+    #     Write-KeyValue $kv $KeyValueColorGreenHexComponentKey $hgreen
+    #     Write-KeyValue $kv $KeyValueColorBlueHexComponentKey $hblue
+    #     Write-KeyValue $kv $KeyValueColorHueComponentKey $hue
+    #     Write-KeyValue $kv $KeyValueColorSaturationComponentKey $saturation
+    #     Write-KeyValue $kv $KeyValueColorBrightnessComponentKey $brightness
+    # # }
+    # Write-SectionClose $kv
+
+    ++$progress
+}
+
+Write-SectionClose $kv
+[void]$kv.Append("`n")
+
+$kv.ToString() | Set-Content -LiteralPath $OutputFilePath -Encoding UTF8 -NoNewLine -Force
+"$([string]::Join("`n", ($includes | Select-Object -ExpandProperty BaseName)))`n" | Set-Content -LiteralPath $ExcludeFilePath -Encoding UTF8 -NoNewLine -Force
+
+$PSStyle.Progress.Style = $DefaultProgressStyle
+$PSStyle.Progress.View = $DefaultProgressView
---
+++ Extract-ImageColor.psm1
@@ -0,0 +1,302 @@
+Set-StrictMode -Version 3.0
+
+function New-Constant {
+    param(
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Name,
+
+        [Parameter(Mandatory)]
+        [AllowNull()]
+        $Value
+    )
+
+    New-Variable -Scope Script -Option Constant -Name $Name -Value $Value
+}
+
+function Get-NameOf {
+    param(
+        [Parameter(Mandatory)]
+        [ScriptBlock]$Expression
+    )
+
+    $element = $Expression.Ast.EndBlock.Statements[0].PipelineElements[0]
+    $elementPath = $element.Expression.VariablePath.UserPath
+    if (-Not $elementPath) {
+        throw 'Could not get the name of the provided expression by assuming it was a variable!'
+    }
+
+    return $elementPath
+}
+
+function Get-InvalidDirectoryPathErrorMessage {
+    param(
+        [Parameter(Mandatory)]
+        [string]$ParamName
+    )
+
+    return 'The specified $' + $ParamName + ' parameter represents an invalid directory path. It could be that the path does not exists, or it is not a directory!'
+}
+
+function Get-InvalidFilePathErrorMessage {
+    param(
+        [Parameter(Mandatory)]
+        [string]$ParamName
+    )
+
+    return 'The specified $' + $ParamName + ' parameter represents an invalid file path. It could be that the path does not exists, or it is not a file!'
+}
+
+function Test-SourceDirectoryPath {
+    param(
+        [Parameter(Mandatory)]
+        [AllowNull()]
+        [AllowEmptyString()]
+        [string]$Value
+    )
+
+    return -Not [string]::IsNullOrWhiteSpace($Value)
+}
+
+function Test-OutputDirectoryPath {
+    param(
+        [Parameter(Mandatory)]
+        [AllowNull()]
+        [AllowEmptyString()]
+        [string]$Value
+    )
+
+    return -Not [string]::IsNullOrWhiteSpace($Value)
+}
+
+function Test-OutputFilePath {
+    param(
+        [Parameter(Mandatory)]
+        [AllowNull()]
+        [AllowEmptyString()]
+        [string]$Value
+    )
+
+    return -Not [string]::IsNullOrWhiteSpace($Value)
+}
+
+function Test-ExcludeFilePath {
+    param(
+        [Parameter(Mandatory)]
+        [AllowNull()]
+        [AllowEmptyString()]
+        [string]$Value
+    )
+
+    return -Not [string]::IsNullOrWhiteSpace($Value)
+}
+
+function Test-SampleColorCount {
+    param(
+        [Parameter(Mandatory)]
+        [AllowNull()]
+        [Nullable[int]]$Value
+    )
+
+    return -Not (($Value -Eq $null) -Or ($Value -Le 0))
+}
+
+function Convert-ToHex {
+    param(
+        [Parameter(Mandatory)]
+        [AllowNull()]
+        [Nullable[float]]$Red,
+
+        [Parameter(Mandatory)]
+        [AllowNull()]
+        [Nullable[float]]$Green,
+
+        [Parameter(Mandatory)]
+        [AllowNull()]
+        [Nullable[float]]$Blue
+    )
+
+    function Normalize-ColorComponent {
+        param(
+            [Parameter(Mandatory)]
+            [AllowNull()]
+            [Nullable[float]]$Value
+        )
+
+        if ($Value -Eq $null) {
+            return 0
+        }
+
+        return [int][Math]::Round([Math]::Clamp([Math]::Abs($Value), 0, 255))
+    }
+
+    function Format-ColorComponent {
+        param(
+            [Parameter(Mandatory)]
+            [AllowNull()]
+            [Nullable[int]]$Value
+        )
+
+        if ($Value -Eq $null) {
+            $Value = 0
+        }
+
+        return '{0:X2}' -F $Value
+    }
+
+    return (Format-ColorComponent (Normalize-ColorComponent $Red)), `
+        (Format-ColorComponent (Normalize-ColorComponent $Green)), `
+        (Format-ColorComponent (Normalize-ColorComponent $Blue))
+}
+
+function Convert-ToHsb {
+    param(
+        [Parameter(Mandatory)]
+        [AllowNull()]
+        [Nullable[float]]$Red,
+
+        [Parameter(Mandatory)]
+        [AllowNull()]
+        [Nullable[float]]$Green,
+
+        [Parameter(Mandatory)]
+        [AllowNull()]
+        [Nullable[float]]$Blue
+    )
+
+    function Normalize-ColorComponent {
+        param(
+            [Parameter(Mandatory)]
+            [AllowNull()]
+            [Nullable[float]]$Value
+        )
+
+        if ($Value -Eq $null) {
+            return 0.f
+        }
+
+        return [Math]::Clamp([Math]::Abs($Value), 0, 255) / 255
+    }
+
+    $Red = Normalize-ColorComponent $Red
+    $Green = Normalize-ColorComponent $Green
+    $Blue = Normalize-ColorComponent $Blue
+
+    [object]$mea = $Red, $Green, $Blue | Measure-Object -Minimum -Maximum
+
+    [float]$min = $mea.Minimum
+    [float]$max = $mea.Maximum
+    [float]$delta = $max - $min
+
+    [float]$h = 0
+    [float]$s = 0
+    [float]$b = $max
+
+    if (($delta -Lt 0.00001) `
+            -Or ($max -Eq 0)) {
+        return $h, $s, $b
+    }
+
+    $s = $delta / $max
+    if ($Red -Eq $max) {
+        $h = ($Green - $Blue) / $delta
+    } elseif ($Green -Eq $max) {
+        $h = 2 + ($Blue - $Red) / $delta
+    } else {
+        $h = 4 + ($Red - $Green) / $delta
+    }
+
+    $h *= 60
+    if ($h -Lt 0) {
+        $h += 360
+    }
+
+    return $h, $s, $b
+}
+
+function Write-SectionStart {
+    param(
+        [Parameter(Mandatory)]
+        [Text.StringBuilder]$Builder,
+
+        [Parameter(Mandatory)]
+        [string]$Name
+    )
+
+    [void]$Builder.Append('"')
+    [void]$Builder.Append($Name)
+    [void]$Builder.Append('"{')
+}
+
+function Write-SectionClose {
+    param(
+        [Parameter(Mandatory)]
+        [Text.StringBuilder]$Builder
+    )
+
+    [void]$Builder.Append('}')
+}
+
+function Write-KeyValue {
+    param(
+        [Parameter(Mandatory)]
+        [Text.StringBuilder]$Builder,
+
+        [Parameter(Mandatory)]
+        [string]$Key,
+
+        [Parameter(Mandatory)]
+        [AllowEmptyString()]
+        [string]$Value
+    )
+
+    [void]$Builder.Append('"')
+    [void]$Builder.Append($Key)
+    [void]$Builder.Append('"')
+    [void]$Builder.Append(' "')
+    [void]$Builder.Append($Value)
+    [void]$Builder.Append('"')
+}
+
+
+New-Constant -Name 'DefaultSourceDirectoryPath' -Value (Join-Path $PSScriptRoot 'maps' 'main')
+New-Constant -Name 'DefaultOutputDirectoryPath' -Value (Join-Path $PSScriptRoot 'maps')
+New-Constant -Name 'DefaultOutputFilePath' -Value 'colors.kv'
+New-Constant -Name 'DefaultExcludeFilePath' -Value (Join-Path $PSScriptRoot 'maps' 'excludes.txt')
+New-Constant -Name 'DefaultSampleColorCount' -Value 4
+
+New-Constant -Name 'DefaultProgressStyle' -Value $PSStyle.Progress.Style
+New-Constant -Name 'DefaultProgressView' -Value $PSStyle.Progress.View
+
+New-Constant -Name 'ProgressStyle' -Value "$([char]0x1b)[38;2;255;199;231m"
+New-Constant -Name 'ProgressView' -Value 'Minimal'
+
+New-Constant -Name 'ProgressImageProcessId' -Value 1
+New-Constant -Name 'ProgressImageProcessActivity' -Value 'No cat~ images! Only boring maps of fictional worlds..'
+New-Constant -Name 'ProgressImageProcessStatus' -Value 'Squashing this dummy {0}..'
+
+New-Constant -Name 'ColorHistogramCountRegexGroup' -Value 'count'
+New-Constant -Name 'ColorHistogramRedComponentRegexGroup' -Value 'red'
+New-Constant -Name 'ColorHistogramGreenComponentRegexGroup' -Value 'green'
+New-Constant -Name 'ColorHistogramBlueComponentRegexGroup' -Value 'blue'
+New-Constant -Name 'ColorHistogramHexRegexGroup' -Value 'hex'
+New-Constant -Name 'ColorHistogramRedHexComponentRegexGroup' -Value 'hred'
+New-Constant -Name 'ColorHistogramGreenHexComponentRegexGroup' -Value 'hgreen'
+New-Constant -Name 'ColorHistogramBlueHexComponentRegexGroup' -Value 'hblue'
+New-Constant -Name 'ColorHistogramRegex' -Value ([Regex]::new("^\s*(?<$($ColorHistogramCountRegexGroup)>\d+):\s*\(\s*(?<$($ColorHistogramRedComponentRegexGroup)>\d+\.\d+)\s*,\s*(?<$($ColorHistogramGreenComponentRegexGroup)>\d+\.\d+)\s*,\s*(?<$($ColorHistogramBlueComponentRegexGroup)>\d+\.\d+)\s*\)\s*#(?<$($ColorHistogramHexRegexGroup)>(?<$($ColorHistogramRedHexComponentRegexGroup)>[0-9A-Fa-f]{2})(?<$($ColorHistogramGreenHexComponentRegexGroup)>[0-9A-Fa-f]{2})(?<$($ColorHistogramBlueHexComponentRegexGroup)>[0-9A-Fa-f]{2}))\s*srgb\(\s*\d+\.\d+%\s*,\s*\d+\.\d+%\s*,\s*\d+\.\d+%\s*\)\s*$"))
+
+New-Constant -Name 'KeyValueColorRootSectionName' -Value 'Map Image Colors'
+New-Constant -Name 'KeyValueColorValueKey' -Value 'Value'
+New-Constant -Name 'KeyValueColorHexKey' -Value 'Hex'
+New-Constant -Name 'KeyValueColorRedComponentKey' -Value 'Red'
+New-Constant -Name 'KeyValueColorGreenComponentKey' -Value 'Green'
+New-Constant -Name 'KeyValueColorBlueComponentKey' -Value 'Blue'
+New-Constant -Name 'KeyValueColorRedHexComponentKey' -Value 'Red Hex'
+New-Constant -Name 'KeyValueColorGreenHexComponentKey' -Value 'Green Hex'
+New-Constant -Name 'KeyValueColorBlueHexComponentKey' -Value 'Blue Hex'
+New-Constant -Name 'KeyValueColorHueComponentKey' -Value 'Hue'
+New-Constant -Name 'KeyValueColorSaturationComponentKey' -Value 'Saturation'
+New-Constant -Name 'KeyValueColorBrightnessComponentKey' -Value 'Brightness'
+
+
+Export-ModuleMember -Function * -Alias * -Variable *
