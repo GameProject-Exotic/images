3e131a5915c561f714d1a06c2732310b4808a8ea
anominy <140955338+anominy@users.noreply.github.com>
2026-07-05 11:25:30 +0300

    Assign GPL-3.0 license to Extract-ImageColor script

--- Extract-ImageColor.ps1
+++ Extract-ImageColor.ps1
@@ -1,3 +1,22 @@
+#Requires -Version 7
+
+# Extract-ImageColor
+#
+# Copyright (C) 2026  anominy
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 param(
     [Alias('SourceDirectory', 'SourceDir', 'SrcDir', 'Source', 'Src')]
     [AllowNull()]
@@ -18,8 +37,6 @@ param(
     [AllowNull()]
     [Nullable[int]]$SampleColorCount = $null
 )
-
-#Requires -Version 7
 
 $ErrorActionPreference = 'Stop'
 $ProgressPreference = 'Continue'
